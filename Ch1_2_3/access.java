511c065fb4dba0371e5d5294e8c00e301d4cab6e
edisons3608 <57467707+edisons3608@users.noreply.github.com>
2022-08-24 13:56:53 -0500

    changes

---
+++ Ch1_2_3/access.java
@@ -0,0 +1,7 @@
+public class access {
+    public static void main(String[] args){
+        Dog dog = new Dog("hi",10);
+        System.out.println(dog.getName());
+        System.out.println("hi");
+    }
+}
---
+++ Ch1_2_3/loops.java
@@ -0,0 +1,12 @@
+public class loops {
+    public static void main(String[] args){
+        for(int x=0;x<10;x++){
+            for(int y=0;y<10;y++){
+                System.out.print("x");
+            }
+            System.out.println();
+        }
+        
+    }
+
+}
